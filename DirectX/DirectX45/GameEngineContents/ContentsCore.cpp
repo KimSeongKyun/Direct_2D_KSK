66e29bdee3d66ee8ab77a0d46770f56a33533e5e
skkim <skkim@DESKTOP-UI6SH7U>
2023-06-21 00:15:02 +0900

    김성균

---
+++ DirectX/DirectX45/GameEngineContents/ContentsCore.cpp
@@ -0,0 +1,37 @@
+#include "PrecompileHeader.h"
+#include "ContentsCore.h"
+#include <GameEngineCore\GameEngineCore.h>
+#include "PlayLevel.h"
+#include "TitleLevel.h"
+#include "TileMapLevel.h"
+#include <GameEngineCore/GameEngineCoreWindow.h>
+
+
+
+ContentsCore::ContentsCore() 
+{
+}
+
+ContentsCore::~ContentsCore() 
+{
+}
+
+
+void ContentsCore::GameStart() 
+{
+
+
+	new int();
+
+	GameEngineGUI::GUIWindowCreate<GameEngineCoreWindow>("CoreWindow");
+	ContentsResourcesCreate();
+	GameEngineCore::CreateLevel<TitleLevel>();
+	GameEngineCore::CreateLevel<PlayLevel>();
+	GameEngineCore::CreateLevel<TileMapLevel>();
+	GameEngineCore::ChangeLevel("TileMapLevel");
+}
+
+void ContentsCore::GameEnd() 
+{
+
+}
